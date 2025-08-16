05503b3f15a120e0f8b808edb394c54116060a3c
Frank William <103291649+frankstiwrx@users.noreply.github.com>
2025-08-15 21:53:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let speech = new SpeechSynthesisUtterance();
+
+let button = document.querySelector("button");
+
+let selectVoice = document.querySelector("select");
+
+let voices = [];
+
+button.addEventListener("click", function () {
+
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+
+});
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, index) => {
+        selectVoice.options[index] = new Option(voice.name, index);
+    });
+};
+
+selectVoice.addEventListener('change', () => {
+  speech.voice = voices[parseInt(selectVoice.value)];
+});
